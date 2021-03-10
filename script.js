3fa01e736080865f8d704776b1e7082678bbe296
Martin <x00135214@gmail.com>
2021-03-10 00:20:12 +0000

    readyState fixed with timeout

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let boardSize = 6; /* 4x3 game -  */
-/*let myCards = [];
-*/
+
 /* call functions on document ready - https://stackoverflow.com/questions/17567176/how-to-call-a-function-inside-document-ready/17567264 */
 $(document).ready(function(){
     // call function to shuffle the cards
     cardURL = shuffle(cardURL);
     writeToDocument();
+       
   });
 /* get data from JSON */
 function getData(cb) {
@@ -46,13 +46,49 @@ function getData(cb) {
         */
         for (let i=0; i<boardSize; i++) {
             let x = cardURL[i];
-            document.getElementById("game-board").innerHTML += '<div class="memory-card" id="card'+i +'" data-framework="card'+i+'"><img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
-            document.getElementById("game-board").innerHTML += '<div class="memory-card" id="card'+i +'" data-framework="card'+i+'"><img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
+            //document.getElementById("game-board").innerHTML += '<div class="memory-card" id="card'+i +'" data-framework="card'+i+'"><img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
+            //document.getElementById("game-board").innerHTML += '<div class="memory-card" id="card'+i +'" data-framework="card'+i+'"><img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
+            let firstDiv = document.createElement('div');
+            let secondDiv = document.createElement('div');
+            const content = document.getElementById('game-board');
+            firstDiv.setAttribute("class","memory-card");
+            firstDiv.setAttribute("id","card"+i);
+            firstDiv.setAttribute("data-framework","card"+i);
+            secondDiv.setAttribute("class","memory-card");
+            secondDiv.setAttribute("id","card"+i);
+            secondDiv.setAttribute("data-framework","card"+i);
+            content.appendChild(firstDiv).innerHTML += '<img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';;
+            content.appendChild(secondDiv).innerHTML += '<img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';;
+            document.querySelectorAll(".card"+i)
+            //.innerHTML += '<div class="memory-card" id="card'+i +'" data-framework="card'+i+'"><img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
+            /*const fakeImages = document.querySelectorAll(".memory-card");
+	        fakeImages.forEach(fakeImage => {
+	            fakeImage.innerHTML += '<img class="front-face" src="'+data[x].url+'" alt="Car logo'+i+'"><img class="back-face" src="./www.pexels.com--photo--yellow-nissan-classic-car-beside-gray-beige-concrete-building-69020.jpg"  alt="JS Badge" />';
+	        });*/
         }
-       
-  });
+    });
 }
+
+    
+
 /* temporary function to clear document for testing code */
 function clearDocument() {
     document.getElementById("demo").innerHTML = "";   
+}
+
+function test() {
+    console.log(this);
+}
+
+/* timeout to fix readyState problem 
+setTimeout(function() {
+const cards = document.querySelectorAll('.memory-card');
+cards.forEach(card => card.addEventListener('click', flipCard));
+},500);
+*/
+
+
+function flipCard() {
+    console.log(this);
+    console.log("click");
 }
